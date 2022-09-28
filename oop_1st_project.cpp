6fa4dc511b42579b1d450d767e5432bd723236c0
Toheedullah-K3 <108226737+Toheedullah-K3@users.noreply.github.com>
2022-09-28 10:31:54 +0500

    Create oop_1st_project.cpp

--- oop_1st_project.cpp
+++ oop_1st_project.cpp
@@ -1,64 +1,69 @@
-
 #include <iostream>
 using namespace std;
 
 
 class Time {
-    
+
     unsigned int hours;
     unsigned int minutes;
     unsigned int seconds;
-    
+
 public:
-    
+
     void setHours(unsigned int h);
     void setMinutes(unsigned int m);
     void setSeconds(unsigned int s);
-    void displayTime(unsigned h , unsigned m , unsigned s);
+    void displayTime(unsigned int h, unsigned int m, unsigned int s);
     Time addTime(Time);
     Time subTime(Time);
     bool compareTime(Time);
-    
-    
+
+
 };
 
 
-void Time::setHours(unsigned int h){
+void Time::setHours(unsigned int h) {
     hours = h;
 }
-    
-void Time::setMinutes(unsigned int m){
+
+void Time::setMinutes(unsigned int m) {
     minutes = m;
 }
-    
-void Time::setSeconds(unsigned int s){
+
+void Time::setSeconds(unsigned int s) {
     seconds = s;
 }
 
 // displaying time
-void Time::displayTime(unsigned int h , unsigned int m , unsigned int s){
-    cout<< "Currently Time is: "<<h << " : " << m << " : " << s;
+void Time::displayTime(unsigned int h, unsigned int m, unsigned int s) {
+    cout << "Currently Time is: " << h << " : " << m << " : " << s;
 }
 
 // adding 
 
-Time Time::addTime(Time t){
+Time Time::addTime(Time t) {
     Time temp;
     temp.seconds = seconds + t.seconds;
     temp.minutes = minutes + t.minutes;
     temp.hours = hours + t.hours;
-    
-    if(temp.seconds >= 60) {
+
+    if (temp.seconds >= 60) {
         temp.minutes += temp.seconds / 60;
         temp.seconds = temp.seconds % 60;
     }
-    
-    if(temp.hours >= 24){
+
+    if (temp.minutes >= 60) {
+        temp.hours += temp.minutes / 60;
+        temp.minutes = temp.minutes % 60;
+    }
+
+    if (temp.hours >= 24) {
         temp.hours = temp.hours % 24;
     }
-    cout<< "\n" << "After Addition Time is:  " <<temp.hours << " : ";
-    cout<< temp.minutes << " : ";
-    cout<< temp.seconds << endl;
+    cout << "\n" << "After Addition Time is:  " << temp.hours << " : ";
+    cout << temp.minutes << " : ";
+    cout << temp.seconds << endl;
+    return temp;
 }
 
 // substract
@@ -73,71 +78,91 @@ Time Time::subTime(Time t) {
     cout << temp.hours << " : ";
     cout << temp.minutes << " : ";
     cout << temp.seconds << endl;
+    return temp;
 }
-    
+
 
 // comparing
 
-bool Time::compareTime(Time t){
-    if(hours == t.hours && minutes == t.minutes && seconds == t.seconds){
+bool Time::compareTime(Time t) {
+    if (hours == t.hours && minutes == t.minutes && seconds == t.seconds) {
         return true;
-    }else{
+    }
+    else {
         return false;
     }
 }
 
 int main() {
-    unsigned int h1,m1,s1,h2,m2,s2;
-    do{
-        
-    cout <<"Hour of time 1" <<endl;
-    cin >> h1;
-    
-    cout <<"Minutes of time 1" <<endl;
-    cin >> m1;
-    
-    cout <<"Seconds of time 1" <<endl;
-    cin >> s1;
-    
-    
-    cout <<"Hour of time 2" <<endl;
-    cin >> h2;
-    
-    cout <<"Minutes of time 2" <<endl;
-    cin >> m2;
-    
-    cout <<"Seconds of time 2" <<endl;
-    cin >> s2;
-  
-    }while((h1 > 24 < h2) || (m1 > 60 < m2) || (s1 > 60 < s2));
-    
-    
+
+    unsigned int h1, m1, s1, h2, m2, s2;
+
+
+    do {
+        cout << "Hour of time 1" << endl;
+        cin >> h1;
+    } while (h1 > 24);
+
+
+
+    do {
+        cout << "Minutes of time 1" << endl;
+        cin >> m1;
+    } while (m1 > 60);
+
+
+
+    do {
+        cout << "Seconds of time 1" << endl;
+        cin >> s1;
+    } while (s1 > 60);
+
+
+
+    do {
+        cout << "Hour of time 2" << endl;
+        cin >> h2;
+    } while (h2 > 24);
+
+
+
+    do {
+        cout << "Minutes of time 2" << endl;
+        cin >> m2;
+    } while (m2 > 60);
+
+
+
+    do {
+        cout << "Seconds of time 2" << endl;
+        cin >> s2;
+    } while (s2 > 60);
+
+
+
     Time t1;
     Time t2;
-    t1.setHours(12);
-    t1.setMinutes(23);
-    t1.setSeconds(52);
-    
-    t1.displayTime(6,3,5);
-    cout<<endl;
-    
-    t2.setHours(6);
-    t2.setMinutes(15);
-    t2.setSeconds(37);
-    
-    t2.displayTime(3,4,5);
-    
+    t1.setHours(h1);
+    t1.setMinutes(m1);
+    t1.setSeconds(s1);
+
+    t1.displayTime(h1, m1, s1);
+    cout << endl;
+
+    t2.setHours(h2);
+    t2.setMinutes(m2);
+    t2.setSeconds(s2);
+
+    t2.displayTime(h2, m2, s2);
+
     t1.addTime(t2);
     t1.subTime(t2);
-    
-    if(t1.compareTime(t2)){
-        cout<<"Time are Equal";
-    }else{
-        cout<<"Not Equal";
+
+    if (t1.compareTime(t2)) {
+        cout << "Time are Equal";
     }
-    
-    
-    
-    
-    
+    else {
+        cout << "Not Equal";
+    }
+
 }
